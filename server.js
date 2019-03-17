e6eeebe04e675a7677c90867fd3f50b76c9d4d5b
Drakeen <info@howardfms.com>
2019-03-16 18:21:46 -0600

    adding routes and mongo schema and connections

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const path = require("path");
+
+//Added by JCMH
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+//We don't need path here cause it's actually now accesed in the routes
+//const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,12 +18,17 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//this is now in the routes index.js file so we don't need it here anymore
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
